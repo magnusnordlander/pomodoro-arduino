f910d33a37640b6e0524f371bb5808499420d363
Magnus Nordlander <magnus@nordlander.se>
2021-03-29 10:41:48 +0200

    Blink

---
+++ Pomodoro.ino
@@ -0,0 +1,32 @@
+// DT and CLK must be interrupt pins
+#define ENCODER_SW_PIN 4
+#define ENCODER_DT_PIN 3
+#define ENCODER_CLK_PIN 2
+
+#define SWITCH_PIN 14
+
+#define ON_BOARD_LED_PIN 21
+
+// With the exception of CS, these pins are hardware supported
+#define UEXT_SPI_CS_PIN 10
+#define UEXT_SPI_SCK_PIN 13
+#define UEXT_SPI_MISO_PIN 12
+#define UEXT_SPI_MOSI_PIN 11
+#define UEXT_I2C_SCL_PIN 19
+#define UEXT_I2C_SDA_PIN 18
+
+void setup() {
+    pinMode(SWITCH_PIN, INPUT_PULLUP);
+    pinMode(ENCODER_SW_PIN, INPUT_PULLUP);
+
+    pinMode(ON_BOARD_LED_PIN, OUTPUT);
+}
+
+
+
+void loop() {
+    digitalWrite(ON_BOARD_LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(ON_BOARD_LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);                       // wait for a second
+}
